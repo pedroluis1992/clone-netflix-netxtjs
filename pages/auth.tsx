1003b17722cb062b3ddcc16ebcf710466682d8b6
pedro <pedro.ayala@keydok.com>
2023-03-27 12:27:35 -0600

    Add prima and data models

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,11 +1,17 @@
 import Input from "@/components/input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
 
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
+  const [variant, setVariant] = useState('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login'  ? 'register' : 'login')
+  }, [])
 
 
   return (
@@ -17,15 +23,17 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
           <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign in
+              {variant === "login" ? "Sign in" : "Register"}
           </h2>
             <div className="flex flex-col gap-4">
-              <Input 
-                label="Username" 
-                onChange={(event: any) => setName(event.target.value)} 
-                id="name"  
-                value={name}  
-              />
+              {variant === 'register' && (
+                <Input 
+                  label="Username" 
+                  onChange={(event: any) => setName(event.target.value)} 
+                  id="name"  
+                  value={name}  
+                />
+              )}
               <Input 
                 label="Email" 
                 onChange={(event: any) => setEmail(event.target.value)} 
@@ -41,8 +49,14 @@ const Auth = () => {
                 value={password}  
               />
               <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                Login
+                {variant === 'login' ? 'Login': 'Sign up'}
               </button>
+              <p className="text-neutral-500 mt-12">
+                {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+                <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                  {variant === 'login' ? 'Create and account' : 'Login'}
+                </span>
+              </p>
             </div>
           </div>
         </div>
