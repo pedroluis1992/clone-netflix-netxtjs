3d4a9807618465bc8fbef89e88ad11dab18e2c88
pedro <pedro.ayala@keydok.com>
2023-04-03 23:17:03 -0600

    Add home view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import Navbar from '@/components/Navbar';
+import useMovieList from '@/hooks/useMovieList';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return {
+      redirect: {
+        destination: 'auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
+
+  const { data: movies = []} = useMovieList();
   return (
     <>
-     <h1> Netflix clone</h1>
+      <Navbar />
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies}/> 
+      </div>
     </>
   )
 }
